918438df8245abc9b27df6a39b09dea8cb2de7b6
dejiplus <admin@dejiplus.net>
2013-02-08 05:11:45 +0900

    use maven

---
+++ src/main/java/net/dejiplus/dejipluscmd/dejiplusCmd.java
@@ -0,0 +1,28 @@
+package net.dejiplus.dejipluscmd;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class dejiplusCmd extends JavaPlugin{
+    
+    public final static Logger log = Logger.getLogger("Minecraft");
+    public final static String logPrefix = "[dejiplusCmd] ";
+    public final static String msgPrefix = "[DC] ";
+    
+    public void onEnable() {
+        
+        PluginDescriptionFile pdfFile = this.getDescription();
+        log.info("[" + pdfFile.getName() + "] version " + pdfFile.getVersion() + " is enabled!");
+        
+    }
+    
+    public void onDisable() {
+        
+        PluginDescriptionFile pdfFile = this.getDescription();
+        log.info("[" + pdfFile.getName() + "} version " + pdfFile.getVersion() + "is disabled!");
+        
+    }
+
+}
