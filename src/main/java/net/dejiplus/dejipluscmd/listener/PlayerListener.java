2ca6f08afbf01b2526e761691c1b2bcaff60773c
dejiplus <admin@dejiplus.net>
2013-02-08 22:31:52 +0900

    [Fix Add] Join/QuitMessage

--- src/main/java/net/dejiplus/dejipluscmd/listener/PlayerListener.java
+++ src/main/java/net/dejiplus/dejipluscmd/listener/PlayerListener.java
@@ -1,10 +1,11 @@
 package net.dejiplus.dejipluscmd.listener;
 
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
@@ -16,7 +17,16 @@ public class PlayerListener implements Listener {
 
         final Player player = event.getPlayer();
 
-        Bukkit.getServer().broadcastMessage( player + "がログインしました。");
+        event.setJoinMessage(ChatColor.AQUA + player.getName() + "がログインしました。");
+
+    }
+
+    @EventHandler
+    public void onPlayerQuit(final PlayerQuitEvent event) {
+
+        final Player player = event.getPlayer();
+
+        event.setQuitMessage(ChatColor.AQUA + player.getName() + "がログアウトしました。");
 
     }
 
