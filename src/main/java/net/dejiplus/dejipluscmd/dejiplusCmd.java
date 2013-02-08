39b1f53ee2dd69af6d6ca7177771dc7b9c7c95fd
dejiplus <admin@dejiplus.net>
2013-02-08 11:24:19 +0900

    Bukkit version up

--- src/main/java/net/dejiplus/dejipluscmd/dejiplusCmd.java
+++ src/main/java/net/dejiplus/dejipluscmd/dejiplusCmd.java
@@ -6,23 +6,24 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class dejiplusCmd extends JavaPlugin{
-    
+
+    // Loggger
     public final static Logger log = Logger.getLogger("Minecraft");
     public final static String logPrefix = "[dejiplusCmd] ";
     public final static String msgPrefix = "[DC] ";
-    
+
     public void onEnable() {
-        
+
         PluginDescriptionFile pdfFile = this.getDescription();
         log.info("[" + pdfFile.getName() + "] version " + pdfFile.getVersion() + " is enabled!");
-        
+
     }
-    
+
     public void onDisable() {
-        
+
         PluginDescriptionFile pdfFile = this.getDescription();
         log.info("[" + pdfFile.getName() + "} version " + pdfFile.getVersion() + "is disabled!");
-        
+
     }
 
 }
